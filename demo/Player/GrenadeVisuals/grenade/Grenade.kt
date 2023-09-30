73482abfc0d792f8b21c3ac6b8bd83ffeb3f3fec
chippmann <cedric.hippmann@hotmail.com>
2023-09-30 16:29:21 +0200

    Implement `Grenade` in kotlin

---
+++ demo/Player/GrenadeVisuals/grenade/Grenade.kt
@@ -0,0 +1,29 @@
+package Player.GrenadeVisuals.grenade
+
+import godot.AnimationPlayer
+import godot.Node3D
+import godot.annotation.Export
+import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
+import godot.annotation.RegisterProperty
+import godot.core.Vector3
+import godot.core.asStringName
+
+@RegisterClass
+class Grenade: Node3D() {
+    @Export
+    @RegisterProperty
+    lateinit var animationPlayer: AnimationPlayer
+
+    private val rotationAxis = Vector3.RIGHT.normalized()
+
+    @RegisterFunction
+    override fun _ready() {
+        animationPlayer.play("wave".asStringName())
+    }
+
+    @RegisterFunction
+    override fun _process(delta: Double) {
+        rotateObjectLocal(rotationAxis, (10 * delta).toFloat())
+    }
+}
