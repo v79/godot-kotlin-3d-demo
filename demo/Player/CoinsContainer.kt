83af8af2baa0e23b1312777de40125a9d4ffd8f1
chippmann <cedric.hippmann@hotmail.com>
2023-10-01 08:25:12 +0200

    Implement `CoinsContainer` in kotlin

---
+++ demo/Player/CoinsContainer.kt
@@ -0,0 +1,44 @@
+package Player
+
+import godot.HBoxContainer
+import godot.Label
+import godot.Timer
+import godot.annotation.Export
+import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
+import godot.annotation.RegisterProperty
+import godot.extensions.asNodePath
+
+private const val HIDDEN_Y_POS = -100
+private const val DISPLAY_Y_POS = 20
+
+@RegisterClass
+class CoinsContainer: HBoxContainer() {
+    @Export
+    @RegisterProperty
+    lateinit var displayTimer: Timer
+
+    @Export
+    @RegisterProperty
+    lateinit var coinsLabel: Label
+
+    @RegisterFunction
+    override fun _ready() {
+        displayTimer.timeout.connect(this, ::onTimeout)
+    }
+
+    @RegisterFunction
+    fun updateCoinsAmount(amount: Int) {
+        if (displayTimer.isStopped()) {
+            createTween()?.tweenProperty(this, "position:y".asNodePath(), DISPLAY_Y_POS, 0.5)
+        }
+
+        displayTimer.start()
+        coinsLabel.text = amount.toString()
+    }
+
+    @RegisterFunction
+    fun onTimeout() {
+        createTween()?.tweenProperty(this, "position:y".asNodePath(), HIDDEN_Y_POS, 0.5)
+    }
+}
